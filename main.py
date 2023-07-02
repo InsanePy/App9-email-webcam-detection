1629a5ed71bc879927187ceeaa7aeccd85c58eb2
StudioCrew-design <dev.studiocrew@gmail.com>
2023-07-02 23:08:06 +0530

    Day 37: Detect moving objects and send email using threading

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import cv2
 import time
 from emailing import send_email
+import glob
+import os
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for img in images:
+        os.remove(img)
+
 while True:
     status = 0
     check, frame = video.read()
@@ -24,19 +34,29 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
-
+    clean_thread = Thread(target=clean_folder)
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
-    print(status_list)
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        #clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        clean_thread.start()
 
     cv2.imshow("Video", frame)
 
@@ -46,3 +66,4 @@ while True:
         break
 
 video.release()
+clean_thread.start()
